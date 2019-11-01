df86e4bb0640f8e5f3a2349184160bdb2c7b4b93
alexandru-tugui <57186440+alexandru-tugui@users.noreply.github.com>
2019-11-01 08:35:32 +0200

    Update T1_E5_TuguiAlexandru.m

--- T1_E5_TuguiAlexandru.m
+++ T1_E5_TuguiAlexandru.m
@@ -6,9 +6,7 @@ plot(t,s,'.-'),xlabel('Timp [s]'),grid
 %modificand pasul vectorului timp la 0.01 si apoi la 0.0002, observam ca
 %graficul pentru cel din urma caz are amplitudinea cu mult mai mare, iar
 %unda are o comportare mai regulata
-%b) perioada pentru 0.0002 este de 0,02s
-%pentru 0.01 perioada nu poate fi calculata din grafic
-%pentru 0.001 peioada este tot de 0.02s
+%b) perioada pentru toate cele 3 cazuti este la fel, 0.02s, intrucat daca modificam pasul nu modificam si perioada functiei.
 %c) 
 %creem semnalul cosinusoidal de frecventa 20
 hold on
